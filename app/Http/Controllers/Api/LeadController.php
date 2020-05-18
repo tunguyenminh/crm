e903cccf66aa4b4c69c5bd121db063d8b51e4028
Nguyễn Khánh Duy <duynk163@gmail.com>
2020-05-18 11:20:35 +0700

    Update

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -2,159 +2,54 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Classes\Common;
 use App\Classes\Reply;
 use App\Http\Controllers\Controller as Controller;
-use App\Models\Campaign;
-use App\Models\FormField;
-use App\Models\FormFieldName;
 use App\Models\Lead;
 use App\Models\LeadData;
-use App\Models\Setting;
-use Carbon\Carbon;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Schema;
-use Froiden\Envato\Traits\AppBoot;
 
 class LeadController extends Controller
 {
-    public function saveLeadData(Request $request)
+    public function saveLeadData(Request $request, $id)
     {
-        $campaign = Campaign::find($request->campaign_id);
-        $this->saveImportedLeads($request, $campaign);
-        return 'success';
-    }
+        $lead = Lead::whereRaw('id', $id)->first();
+        $campaign = $lead->campaign;
 
-    private function saveImportedLeads($request, $campaign)
-    {
-        $submittedFormFields = json_decode($request->lead_form_fields);
-        $importType = $request->import_type;
-        $formFields = FormField::where('form_id', $campaign->form_id)->orderBy('order', 'asc')->pluck('field_name', 'id');
-        $leadFormFields = [];
-
-        foreach ($submittedFormFields as $submittedFormFieldsKey => $submittedFormFieldValue)
+        // Saving Lead related data
+        $lead->interested = $request->interested == '' ? null : $request->interested;
+        $lead->email_template_id = ($request->send_email == '' || $request->send_email == 'new') ? null : $request->send_email;
+        $lead->appointment_booked = $request->book_appointment;
+        $lead->time_taken = $request->time_taken;
+        if(!$campaign->auto_reference)
         {
-            if($submittedFormFieldValue > 0)
-            {
-                $leadFormFields[] = $formFields[$submittedFormFieldValue];
-            } else {
-                $leadFormFields[] = $submittedFormFieldValue;
-            }
+            $lead->reference_number = $request->reference_number;
         }
 
-        if($importType == 'text')
-        {
-            $text = trim($request->import_text);
-            $textArray = explode("\n", $text);
-            $textArray = array_filter($textArray, 'trim'); // remove any extra \r characters left behind
-            $row = 0;
+        $lead->first_actioned_by = $lead->first_actioned_by === null ? $this->user->id : $lead->first_actioned_by;
+        $lead->last_actioned_by = $lead->last_actioned_by === null ? $this->user->id : $lead->last_actioned_by;
+        $lead->status = 'actioned';
 
-            foreach ($textArray as $line) {
-                $lineArrayResults = str_getcsv($line);
+        // TODO - Update timer
+        $lead->save();
 
-                if($row > 0)
-                {
-                    $this->checkAndSaveLead($lineArrayResults, $campaign, $submittedFormFields, $formFields);
-                }
 
-                $row++;
-            }
-        }else if($importType == 'file')
-        {
-            $file = fopen($request->import_file, 'r');
-            $row = 0;
-
-            while(($lineArrayResults = fgetcsv($file)) !== FALSE)
-            {
-                // Skip first row
-                if($row > 0)
-                {
-                    $this->checkAndSaveLead($lineArrayResults, $campaign, $submittedFormFields, $formFields);
-                }
-
-                $row++;
-            }
-
-            fclose($file);
-
-        }
-
-        // Saving Total Lead For Campaign
-        $totalCampaignLeads = Lead::where('campaign_id', $campaign->id)->count();
-        $campaign->total_leads =$totalCampaignLeads;
-
-        // Saving remaining lead for campaign
+        $totalLeads = Lead::where('campaign_id', $campaign->id)->count();
         $actionedLeads = Lead::where('campaign_id', $campaign->id)
             ->where('status', 'actioned')->count();
-        $remainingLeads = $totalCampaignLeads - $actionedLeads;
-        $campaign->total_leads = $totalCampaignLeads;
+        $remainingLeads = $totalLeads - $actionedLeads;
+        $campaign->total_leads = $totalLeads;
         $campaign->remaining_leads = $remainingLeads;
         $campaign->save();
-    }
 
-    public function checkAndSaveLead($lineArrayResults, $campaign, $submittedFormFields, $formFields)
-    {
-        // Creating array from csv/txt data
-        $newLeadDataArray = [];
-        foreach ($formFields as $formFieldKey => $formFieldValue)
+        // Saving Lead Data
+        $fields = $request->fields;
+        foreach ($fields as $key => $field)
         {
-            $newLeadDataArray[$formFieldKey] = [
-                'field_name' => $formFieldValue,
-                'field_value' => ''
-            ];
+            $leadData = LeadData::find($key);
+            $leadData->field_value = $field ?? '';
+            $leadData->save();
         }
 
-        foreach ($lineArrayResults as $lineArrayResultKey => $lineArrayResult)
-        {
-            $currentFormFieldId = $submittedFormFields[$lineArrayResultKey];
-            if($currentFormFieldId > 0)
-            {
-                $newLeadDataArray[$currentFormFieldId] = [
-                    'field_name' => $formFields[$currentFormFieldId],
-                    'field_value' => $lineArrayResult
-                ];
-            }
-        }
-
-        // Generating field string
-        $fieldValueString = '';
-        foreach ($formFields as $formFieldKey => $formFieldValue)
-        {
-            if($newLeadDataArray[$formFieldKey]['field_value'] != '')
-            {
-                $fieldValueString .= strtolower($newLeadDataArray[$formFieldKey]['field_value']);
-            }
-        }
-
-        // Checking created hash exists in database
-        $fieldValueStringHash = md5($fieldValueString.$campaign->id);
-        $hashCheck = Lead::where('hash', $fieldValueStringHash)->count();
-        if($hashCheck == 0)
-        {
-            // Creating Lead
-            $lead = new Lead();
-            $lead->reference_number = $campaign->auto_reference == 1 ? $campaign->auto_reference_prefix . '_' . Carbon::now()->timestamp : null;
-            $lead->campaign_id = $campaign->id;
-            $lead->hash = $fieldValueStringHash;
-            $lead->save();
-
-            // Saving Lead Data
-            foreach ($formFields as $formFieldKey => $formFieldValue)
-            {
-//                if($newLeadDataArray[$formFieldKey]['field_value'] != '')
-//                {
-                $leadData = new LeadData();
-                $leadData->lead_id = $lead->id;
-                $leadData->form_field_id = $formFieldKey;
-                $leadData->field_name = $formFieldValue;
-                $leadData->field_value = $newLeadDataArray[$formFieldKey]['field_value'];
-                $leadData->save();
-//                }
-            }
-
-        }
+        return Reply::success('messages.updateSuccess');
     }
 }
