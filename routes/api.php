2e11b2eb3929c5fe282ca34462b3b1a6f62b0d12
Nguyễn Khánh Duy <duynk163@gmail.com>
2020-05-18 11:11:48 +0700

    UpdatE

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/lead', ['as' => 'api.lead', 'uses' => 'Api\LeadController@saveLeadData']);
+Route::post('/lead', ['as' => 'api.lead', 'uses' => 'Api\LeadController@saveLeadData']);
